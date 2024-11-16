8add76e6dedc20a8bd1904417093e394edfea718
Vladislav Latanov <lvn1722@yandex.ru>
2024-11-16 11:53:31 +0300

    practice with strings

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string heroName;
+            int heroHeight;
+            float heroWeight;
+
+            Console.WriteLine("Заполните информацию про своего персонажа!");
+            Console.Write("Введите имя персонажа: ");
+            heroName = Console.ReadLine();
+            Console.Write("Рост персонажа: ");
+            heroHeight = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Вес персонажа: ");
+            heroWeight = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine($"Вашего персонажа зовут {heroName}, его рост - {heroHeight}, а вес - {heroWeight}");
+        }
+    }
+}
---
+++ Strings/Strings/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Strings")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Strings")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("15257fec-4fd4-4efb-b8b5-03399675fb43")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
