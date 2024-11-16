76a73aec43927480f1b7e1725d16c1488fee880a
Vladislav Latanov <lvn1722@yandex.ru>
2024-11-16 20:20:22 +0300

    string variables swapper

---
+++ VariablesSwap/VariablesSwap/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VariablesSwap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Doe";
+            string lastName = "John";
+
+            Console.WriteLine($"До перестановки: Имя - {firstName}, Фамилия - {lastName}");
+
+            string swapVariable = firstName;
+            firstName = lastName;
+            lastName = swapVariable;
+
+            Console.WriteLine($"После перестановки: Имя - {firstName}, Фамилия - {lastName}");
+        }
+    }
+}
