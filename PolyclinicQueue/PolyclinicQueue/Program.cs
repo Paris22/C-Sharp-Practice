6d4476ea887b8c3adfea82fd59666a7d475567d1
Vladislav Latanov <lvn1722@yandex.ru>
2024-11-16 21:10:16 +0300

    task: calculate polyclinic queue time

---
+++ PolyclinicQueue/PolyclinicQueue/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PolyclinicQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int personsNumber;
+            int timePerPerson = 10;
+
+            int hoursInQueue;
+            int minutesInQueue;
+
+            int minutesToHoursConverter = 60;
+
+            Console.Write("Вы приходите в поликлинику и видите очередь... Кол-во людей в очереди: ");
+            personsNumber = Convert.ToInt32(Console.ReadLine());
+
+            hoursInQueue = personsNumber * timePerPerson / minutesToHoursConverter;
+            minutesInQueue = personsNumber * timePerPerson % minutesToHoursConverter;
+
+            Console.WriteLine($"Время, которое Вам нужно отстоять в очереди: кол-во часов - {hoursInQueue}, кол-во минут - {minutesInQueue}");
+        }
+    }
+}
