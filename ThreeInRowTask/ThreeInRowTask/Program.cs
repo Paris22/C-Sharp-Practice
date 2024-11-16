a7c2159c092799c1db15f806d7b0bd42b849c81f
Vladislav Latanov <lvn1722@yandex.ru>
2024-11-16 12:57:44 +0300

    three in row task

---
+++ ThreeInRowTask/ThreeInRowTask/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ThreeInRowTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int imagesInRowNumber = 3;
+            int imagesInAlbumNumber = 52;
+            int filledRows;
+            int excessiveImages;
+
+            filledRows = imagesInAlbumNumber / imagesInRowNumber;
+            excessiveImages = imagesInAlbumNumber % imagesInRowNumber;
+
+            Console.WriteLine($"Полностью заполненных рядов в альбоме: {filledRows}");
+            Console.WriteLine($"Картинок сверх меры: {excessiveImages}");
+        }
+    }
+}
