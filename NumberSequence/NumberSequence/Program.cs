07045be0da111719e638109bab6fd4979722fab1
Vladislav Latanov <lvn1722@yandex.ru>
2024-11-18 22:08:38 +0300

    create number sequence

---
+++ NumberSequence/NumberSequence/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NumberSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberSequenceStart = 5;
+            int numberSequenceEnd = 103;
+            int numberSequenceStep = 7;
+
+            for (int i = numberSequenceStart; i <= numberSequenceEnd; i += numberSequenceStep)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
