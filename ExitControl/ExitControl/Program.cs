d740d25ca8f4b9f70c52afbec842bbb715b86247
Vladislav Latanov <lvn1722@yandex.ru>
2024-11-18 21:55:26 +0300

    exit control from cycle

---
+++ ExitControl/ExitControl/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExitControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputMessage;
+
+            Console.WriteLine("Введите сообщение. Если не хотите больше вводить сообщение, то введите 'exit'");
+            inputMessage = Console.ReadLine();
+
+            while (inputMessage != "exit")
+            {
+                Console.WriteLine("Введите сообщение. Если не хотите больше вводить сообщение, то введите 'exit'");
+            }
+        }
+    }
+}
