e720bbfe70bd99025926316fc7e66a61f45bebb3
Vladislav Latanov <lvn1722@yandex.ru>
2024-11-18 22:28:53 +0300

    numbers sum with condition

---
+++ NumbersSumWithCondition/NumbersSumWithCondition/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NumbersSumWithCondition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int numbersSum = 0;
+            int number = rand.Next(100);
+
+            for (int i = 0; i <= number; i++) {
+                if (i % 3 == 0 || i % 5 == 0) {
+                    numbersSum += i;
+                }
+            }
+
+            Console.WriteLine($"Сумма всех положительных чисел меньше или равных {number}, которые кратны 3 или 5 - {numbersSum}");
+        }
+    }
+}
