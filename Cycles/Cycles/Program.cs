99c1303447e3cac3491aca04be3369518f2afac3
Vladislav Latanov <lvn1722@yandex.ru>
2024-11-18 21:41:36 +0300

    work with for cycle

---
+++ Cycles/Cycles/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Cycles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputMessage;
+            int messageRepeatNumber;
+
+            Console.Write("Введите сообщение, которое хотите вывести повторно ");
+            inputMessage = Console.ReadLine();
+            Console.Write("Введите количество повторений вашего сообщения ");
+            messageRepeatNumber = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < messageRepeatNumber; i++) {
+                Console.WriteLine(inputMessage);
+            }
+        }
+    }
+}
---
+++ Cycles/Cycles/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Cycles")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cycles")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ea8a6329-a7b3-40e7-84a6-4967e409e405")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
