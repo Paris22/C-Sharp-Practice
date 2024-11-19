7e4afb14c881254c313be9795e0e2135280b337f
Vladislav Latanov <lvn1722@yandex.ru>
2024-11-19 14:22:04 +0300

    numbers sum with condition: change code style

--- NumbersSumWithCondition/NumbersSumWithCondition/Program.cs
+++ NumbersSumWithCondition/NumbersSumWithCondition/Program.cs
@@ -14,8 +14,10 @@ namespace NumbersSumWithCondition
             int secondMultiplicityConditionNumber = 5;
             int number = random.Next(numberRandomLimit);
 
-            for (int i = 0; i <= number; i++) {
-                if (i % firstMultiplicityConditionNumber == 0 || i % secondMultiplicityConditionNumber == 0) {
+            for (int i = 0; i <= number; i++)
+            {
+                if (i % firstMultiplicityConditionNumber == 0 || i % secondMultiplicityConditionNumber == 0)
+                {
                     numbersSum += i;
                 }
             }
