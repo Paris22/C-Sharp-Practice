e4de57c60abc4fbd2baad644a6390b46b920d11a
Vladislav Latanov <lvn1722@yandex.ru>
2024-11-23 23:42:22 +0300

    name output wtih borders

---
+++ NameOutput/NameOutput/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NameOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInputName;
+            string userInputBorderChar;
+
+            string nameLine;
+            string borderLine = "";
+
+            Console.WriteLine("Введите свое имя!");
+            userInputName = Console.ReadLine();
+            Console.WriteLine("Введите символ, из которого будет сделан прямоугольник вокруг имени!");
+            userInputBorderChar = Console.ReadLine();
+
+            int borderWidth = userInputName.Length + 2;
+
+            for (int i = 0; i < borderWidth; i++)
+            {
+                borderLine += userInputBorderChar;
+            }
+
+            nameLine = $"{userInputBorderChar}{userInputName}{userInputBorderChar}";
+
+            Console.WriteLine(borderLine);
+            Console.WriteLine(nameLine);
+            Console.WriteLine(borderLine);
+        }
+    }
+}
