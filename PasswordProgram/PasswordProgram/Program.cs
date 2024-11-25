014636635fb39fc03f652895778570f4213c6537
Vladislav Latanov <lvn1722@yandex.ru>
2024-11-26 00:06:27 +0300

    password program: access to info with password

--- PasswordProgram/PasswordProgram/Program.cs
+++ PasswordProgram/PasswordProgram/Program.cs
@@ -1,9 +1,36 @@
-﻿namespace PasswordProgram
+﻿using System;
+
+namespace PasswordProgram
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string userPassword = "qwerty12345";
+            string userInput;
+            string secretMessage = "Секретное сообщение";
+
+            int tryCounter = 0;
+            int maxTries = 3;
+
+            Console.WriteLine("Введите пароль!");
+
+            while (tryCounter < maxTries)
+            {
+                userInput = Console.ReadLine();
+
+                if (userPassword == userInput)
+                {
+                    Console.WriteLine(secretMessage);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный пароль. Ввведите пароль еще раз!");
+                    tryCounter++;
+                }
+
+            }
         }
     }
 }
