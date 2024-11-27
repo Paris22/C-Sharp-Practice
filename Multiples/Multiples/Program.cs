98e68e61c63dfcf94f792451c0cdc695962a37fe
Vladislav Latanov <lvn1722@yandex.ru>
2024-11-27 23:45:49 +0300

    multiples: update solution

--- Multiples/Multiples/Program.cs
+++ Multiples/Multiples/Program.cs
@@ -1,9 +1,41 @@
-﻿namespace Multiples
+﻿using System;
+
+namespace Multiples
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int N = random.Next(10, 26);
+            Console.WriteLine($"Сгенерированное число N: {N}");
+
+            int count = 0;
+            for (int i = 50; i <= 150; i++)
+            {
+
+                if (IsMultiple(i, N))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Количество чисел от 50 до 150, кратных {N}: {count}");
+        }
+
+        static bool IsMultiple(int number, int divisor)
+        {
+
+            int current = divisor;
+            while (current <= number)
+            {
+                if (current == number)
+                {
+                    return true;
+                }
+                current += divisor;
+            }
+            return false;
         }
     }
 }
