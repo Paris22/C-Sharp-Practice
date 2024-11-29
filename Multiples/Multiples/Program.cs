1a35f08b668da2b178b9673f29d539042a4f7bea
Vladislav Latanov <lvn1722@yandex.ru>
2024-11-30 00:09:38 +0300

    multiples: update code style

--- Multiples/Multiples/Program.cs
+++ Multiples/Multiples/Program.cs
@@ -7,25 +7,23 @@ namespace Multiples
         static void Main(string[] args)
         {
             Random random = new Random();
-            int N = random.Next(10, 26);
+            int generatedNumber = random.Next(10, 26);
 
             int multipleCount = 0;
             int numberRangeStart = 50;
             int numberRangeEnd = 150;
 
-            Console.WriteLine($"Сгенерированное число N: {N}");
+            Console.WriteLine($"Сгенерированное число N: {generatedNumber}");
 
-            for (int multiple = 0; multiple <= numberRangeEnd; multiple += N)
+            for (int multiple = 0; multiple <= numberRangeEnd; multiple += generatedNumber)
             {
-
                 if (multiple >= numberRangeStart)
                 {
                     multipleCount++;
                 }
-
             }
 
-            Console.WriteLine($"Количество чисел от 50 до 150, кратных {N}: {multipleCount}");
+            Console.WriteLine($"Количество чисел от {numberRangeStart} до {numberRangeEnd}, кратных {generatedNumber}: {multipleCount}");
         }
     }
 }
