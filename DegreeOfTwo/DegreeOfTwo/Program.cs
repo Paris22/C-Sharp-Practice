797a74ed884b9221f8fa543c991a273339539ba4
Vladislav Latanov <lvn1722@yandex.ru>
2024-11-30 11:47:52 +0300

    DegreeOfTwo: solution

---
+++ DegreeOfTwo/DegreeOfTwo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DegreeOfTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomGeneratedNumber = random.Next(256);
+
+            int degreeOfTwo = 0;
+            int numberInDegree = 1;
+
+            while (numberInDegree <= randomGeneratedNumber)
+            {
+                degreeOfTwo++;
+                numberInDegree *= 2;
+            }
+
+            Console.WriteLine($"Заданное рандомом число: {randomGeneratedNumber}, число превосходящее заданное: {numberInDegree}, степень двойки превосходящего числа {degreeOfTwo}");
+        }
+    }
+}
