140eb125c707039b7642d0919113499d9ba6c31a
Vladislav Latanov <lvn1722@yandex.ru>
2024-12-08 23:25:41 +0300

    SpecificColumnRow: change code style

--- SpecificColumnRow/SpecificColumnRow/Program.cs
+++ SpecificColumnRow/SpecificColumnRow/Program.cs
@@ -9,20 +9,20 @@ namespace SpecificColumnRow
             {
                 int[,] multiDimesionalArray = { { 1, 2, 3 }, { 4, 5, 6 } };
 
-
                 int sumSecondRow = multiDimesionalArray[1, 0] + multiDimesionalArray[1, 1] + multiDimesionalArray[1, 2];
                 int productFirstColumn = multiDimesionalArray[0, 0] * multiDimesionalArray[1, 0];
 
-
                 Console.WriteLine("Исходная матрица:");
-                for (int i = 0; i < multiDimesionalArray.GetLength(0); i++)
+
+                for (int rows = 0; rows < multiDimesionalArray.GetLength(0); rows++)
                 {
-                    for (int j = 0; j < multiDimesionalArray.GetLength(1); j++)
+                    for (int columns = 0; columns < multiDimesionalArray.GetLength(1); columns++)
                     {
-                        Console.Write(multiDimesionalArray[i, j] + " ");
+                        Console.Write(multiDimesionalArray[rows, columns] + " ");
                     }
                     Console.WriteLine();
                 }
+
                 Console.WriteLine("Сумма второй строки: " + sumSecondRow);
                 Console.WriteLine("Произведение первого столбца: " + productFirstColumn);
             }
