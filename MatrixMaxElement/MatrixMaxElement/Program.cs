67570ca8e77073072a71a4d6acab49a0c07e7016
Vladislav Latanov <lvn1722@yandex.ru>
2024-12-08 23:24:08 +0300

    MatrixMaxElement: create matrix with random numbers

--- MatrixMaxElement/MatrixMaxElement/Program.cs
+++ MatrixMaxElement/MatrixMaxElement/Program.cs
@@ -1,10 +1,25 @@
+using System;
+
 namespace MatrixMaxElement
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           int matrix = [,];
+            int[,] matrix = new int[10, 10];
+
+            Random random = new Random();
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int columns = 0; columns < matrix.GetLength(1); columns++)
+                {
+                    matrix[rows, columns] = random.Next(10);
+                    Console.Write(matrix[rows, columns] + " ");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
