481244b85bc19c63f0abc96f78298e5ab88cbc56
Vladislav Latanov <lvn1722@yandex.ru>
2024-12-09 23:58:45 +0300

    MatrixMaxElement: update solution

--- MatrixMaxElement/MatrixMaxElement/Program.cs
+++ MatrixMaxElement/MatrixMaxElement/Program.cs
@@ -6,15 +6,55 @@ namespace MatrixMaxElement
     {
         static void Main(string[] args)
         {
-            int[,] matrix = new int[10, 10];
+            int rowsNumber = 10;
+            int columnsNumber = 10;
+            int[,] matrix = new int[rowsNumber, columnsNumber];
+            int maxElement = int.MinValue;
 
             Random random = new Random();
+            int randomLimit = 10;
+
+            Console.WriteLine("Исходная матрица:");
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int columns = 0; columns < matrix.GetLength(1); columns++)
+                {
+                    matrix[rows, columns] = random.Next(randomLimit);
+                    Console.Write(matrix[rows, columns] + " ");
+                }
+
+                Console.WriteLine();
+            }
 
             for (int rows = 0; rows < matrix.GetLength(0); rows++)
             {
                 for (int columns = 0; columns < matrix.GetLength(1); columns++)
                 {
-                    matrix[rows, columns] = random.Next(10);
+                    if (matrix[rows, columns] > maxElement)
+                    {
+                        maxElement = matrix[rows, columns];
+                    }
+                }
+            }
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int columns = 0; columns < matrix.GetLength(1); columns++)
+                {
+                    if (matrix[rows, columns] == maxElement)
+                    {
+                        matrix[rows, columns] = 0;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Максимальный элемент: {maxElement}");
+
+            Console.WriteLine("Матрица после замены максимального элемента на ноль:");
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int columns = 0; columns < matrix.GetLength(1); columns++)
+                {
                     Console.Write(matrix[rows, columns] + " ");
                 }
 
