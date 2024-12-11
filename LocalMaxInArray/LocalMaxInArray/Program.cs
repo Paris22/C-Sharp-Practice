167926b6b1cf9ab7e20601bdaca662c34fdd90c8
Vladislav Latanov <lvn1722@yandex.ru>
2024-12-11 21:19:29 +0300

    LocalMaxInArray: init with array

---
+++ LocalMaxInArray/LocalMaxInArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LocalMaxInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int arrayLength = 30;
+            int[] initArray = new int[arrayLength];
+            int[] localMaxArray;
+
+            Random random = new Random();
+
+            for (int index = 0; index < initArray.Length; index++)
+            {
+                initArray[index] = random.Next();
+            }
+
+            for (int index = 0; index < initArray.Length; index++)
+            {
+                if (initArray[index] > initArray[index - 1] && initArray[index] > initArray[index + 1])
+                {
+
+                }
+            }
+        }
+    }
+}
