9295f6ae7db8daf577b46df0f67e59440ca7c88b
Vladislav Latanov <lvn1722@yandex.ru>
2024-12-12 21:30:51 +0300

    LocalMaxInArray: fix the limits for the loop and code style

--- LocalMaxInArray/LocalMaxInArray/Program.cs
+++ LocalMaxInArray/LocalMaxInArray/Program.cs
@@ -11,28 +11,32 @@ namespace LocalMaxInArray
             int initArrayLength = initArray.Length;
 
             Random random = new Random();
+            int randomLimit = 10;
 
+            Console.Write("Исходный массив: ");
             for (int index = 0; index < initArray.Length; index++)
             {
-                initArray[index] = random.Next();
+                initArray[index] = random.Next(randomLimit);
+                Console.Write(initArray[index]);
             }
 
+            Console.Write("\nЛокальные максимумы: ");
             if (initArrayLength > 1 && initArray[0] > initArray[1])
             {
-                Console.WriteLine(initArray[0]);
+                Console.Write(initArray[0]);
             }
 
-            for (int index = 0; index < initArrayLength; index++)
+            for (int index = 1; index < initArrayLength - 1; index++)
             {
                 if (initArray[index] > initArray[index - 1] && initArray[index] > initArray[index + 1])
                 {
-                    Console.WriteLine(initArray[index]);
+                    Console.Write(initArray[index]);
                 }
             }
 
             if (initArrayLength > 1 && initArray[initArrayLength - 1] > initArray[initArrayLength - 2])
             {
-                Console.WriteLine(initArray[initArrayLength - 1]);
+                Console.Write(initArray[initArrayLength - 1]);
             }
         }
     }
